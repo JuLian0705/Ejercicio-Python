767dec1e7c428a3501a91eca89d8c3baf74f6831
JuLian0705 <131489864+JuLian0705@users.noreply.github.com>
2025-05-22 00:59:34 -0500

    Add files via upload

---
+++ Ejercicio61.py
@@ -0,0 +1,7 @@
+canguro_meses_por_año = 20
+canguro_por_semanas = 6
+
+fraccion_de_año = 1 / 4
+
+total_semanas = fraccion_de_año * canguro_meses_por_año * canguro_por_semanas
+print(f"En un cuarto de canguro-año hay {total_semanas} canguro-semanas.")
